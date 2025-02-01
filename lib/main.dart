cca0c45e143ee778082c45d4852075257d7a3856
The_Dead_Maus <sbbrcse@gmail.com>
2025-02-01 13:46:53 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,22 +9,87 @@ void main()
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ContactList()
-    );
-
+  State<StatefulWidget> createState() {
+    return ContactList();
   }
 }
 
-class ContactList extends StatefulWidget {
-  @override
-  ContactList createState() => ContactList();
-}
+class ContactList extends State<MyApp> {
 
-class ContactList extends state<>
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Contact List'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextField(
+                controller: nameContact,
+                decoration: InputDecoration(labelText: 'Name',
+                border: OutlineInputBorder(),
+                helperText: 'Enter the name of the contact'),
+                keyboardType: TextInputType.text,
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: phoneContact,
+                decoration: InputDecoration(labelText: 'Phone',
+                border: OutlineInputBorder(),
+                helperText: 'Enter the phone of the contact'),
+                keyboardType: TextInputType.number,
+                cursorColor: Colors.blueGrey,
+                maxLength: 11,
+              ),
+              SizedBox(height: 16),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: addContact,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                  ),
+                  child: Text('Add Contact',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+final nameContact = TextEditingController();
+final phoneContact = TextEditingController();  List<Map<String, String>> contacts = [];
+
+  void addContact()
+  {
+    final name = nameContact.text.trim();
+    final phone = phoneContact.text.trim();
+
+    if(name.isNotEmpty && phone.isNotEmpty)
+    {
+      setState(() {
+        contacts.add(
+            {
+              'name' : name,
+              'phone' : phone
+            });
+      });
+    }
+  }
+
+
+}
 
